6e12e9002cccaca63defe9f71c47073b34500dfb
Koston Zhuang <koston.zhuang@maiscrm.com>
2023-11-29 13:02:10 +0800

    i18next: init

---
+++ docs/i18next/index.js
@@ -0,0 +1,17 @@
+module.exports.default = {
+  sidebar: {
+    '/i18next': [
+      {
+        children: [
+          '/i18next/docs/basic.md',
+        ],
+        text: 'i18next',
+      },
+    ],
+  },
+  navbar: {
+    text: 'i18next',
+    link: '/i18next',
+    group: '前端',
+  },
+};
