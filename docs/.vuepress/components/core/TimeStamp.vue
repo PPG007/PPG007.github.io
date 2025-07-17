3c468392cbe15daa17a52daafe032cc6741903c2
koston.zhuang <koston.zhuang@maiscrm.com>
2025-07-17 10:41:52 +0800

    tools: fix time component

--- docs/.vuepress/components/core/TimeStamp.vue
+++ docs/.vuepress/components/core/TimeStamp.vue
@@ -4,6 +4,7 @@
       <el-select v-model="unit" size="large">
         <el-option label="秒" value="s" />
         <el-option label="毫秒" value="ms" />
+        <el-option v-if="readonly" label="ISO8601" value="iso" />
       </el-select>
     </template>
   </el-input>
@@ -12,33 +13,45 @@
 <script setup lang="ts">
 import { ref, computed, watch } from 'vue';
 import { formatTimestamp, formatDate } from '../utils';
-type unitType = 'ms' | 's';
-const props = withDefaults(defineProps<{
-  value?: Date | number;
-  disableUnit?: boolean;
-}>(), {
-  disableUnit: false,
-  value: 0,
-})
-const unit = ref<unitType>('s');
+import { TimeUnit } from '../types';
+const props = withDefaults(
+  defineProps<{
+    value?: Date | number;
+    disableUnit?: boolean;
+  }>(),
+  {
+    disableUnit: false,
+    value: 0,
+  }
+);
+const unit = ref<TimeUnit>('s');
 const inputText = ref(formatTimestamp(props.value, unit.value));
-watch(() => props.value, () => {
-  inputText.value = formatTimestamp(props.value, unit.value);
-});
-const readonly = computed(() => (props.value instanceof Date));
+watch(
+  () => props.value,
+  () => {
+    inputText.value = formatTimestamp(props.value, unit.value);
+  }
+);
+const readonly = computed(() => props.value instanceof Date);
 const emit = defineEmits<{
   (event: 'change', value: Date): void;
-}>()
+}>();
 watch([inputText, unit], ([newInputText, newUnit]) => {
   if (props.value instanceof Date) {
+    inputText.value = formatTimestamp(props.value, newUnit);
     return;
   }
-  emit('change', formatDate(newInputText, newUnit));
+  // try convert to number
+  const num = Number(newInputText);
+  if (isNaN(num)) {
+    return;
+  }
+  emit('change', formatDate(num, newUnit));
 });
 </script>
 
 <style scoped>
 .el-select {
-  width: 80px;
+  width: 110px;
 }
 </style>
