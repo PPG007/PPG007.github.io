062916d843712bc3b2b7ec4aa58d228fd796b461
PPG007 <1658272229@qq.com>
2026-05-07 21:29:38 +0800

    fastapi: init

---
+++ docs/python/fastapi/index.ts
@@ -0,0 +1,15 @@
+import { BarConfig } from '@/types';
+
+const config: BarConfig = {
+  sidebars: [
+    {
+      children: [],
+    },
+  ],
+  text: 'FastAPI',
+  icon: 'devicon:fastapi',
+  dir: __dirname,
+  devMode: true,
+};
+
+export default config;
--- docs/python/index.ts
+++ docs/python/index.ts
@@ -1,11 +1,12 @@
 import { GroupConfig } from '@/types';
 import basic from './basic';
+import fastapi from './fastapi';
 
 const config: GroupConfig = {
   text: 'Python',
   icon: 'logos:python',
   dir: __dirname,
-  children: [basic],
+  children: [basic, fastapi],
 };
 
 export default config;
